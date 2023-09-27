c021135846867778c18e70d3907b168eacd6dba7
doanduc2000 <doanminhducvip2000@gmail.com>
2023-09-27 15:23:42 +0700

    first commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use Exception;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -14,6 +16,19 @@ class PostController extends Controller
     public function index()
     {
         //
+        try {
+            $post = Post::all();
+            return response()->json([
+                'status' => true,
+                'message' => 'Success',
+                'data' => $post
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => $e
+            ], 500);
+        }
     }
 
     /**
@@ -21,9 +36,28 @@ class PostController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request)
     {
         //
+        try {
+            $query = $request->query();
+            $ip = $request->ip();
+            $post = Post::where('ip', '=', $ip)->where('url', '=', $query['url'])->first();
+            $view = Post::where('url', '=', $query['url'])->get();
+            if ($post !== null) {
+                $post['view_number'] = count($view);
+            }
+            return response()->json([
+                'status' => true,
+                'message' => 'Success',
+                'data' =>  $post
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => $e
+            ], 500);
+        }
     }
 
     /**
@@ -34,7 +68,28 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $input = $request->all();
+            $ip = $request->ip();
+            $post = Post::where('ip', '=', $ip)->get();
+            if (count($post) !== 0) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Ip already exist',
+                ], 400);
+            }
+            $newPost = Post::create(['title' => $input['title'], 'url' => $input['url'], 'ip' =>  $ip]);
+            return response()->json([
+                'status' => true,
+                'message' => 'Success',
+                'data' => $newPost
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => $e
+            ], 500);
+        }
     }
 
     /**
@@ -69,6 +124,33 @@ class PostController extends Controller
     public function update(Request $request, $id)
     {
         //
+        try {
+            $input = $request->all();
+            $post = Post::find($id);
+            if (!$post) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Post not found',
+                ], 400);
+            }
+            if (!array_key_exists('liked', $input)) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Key liked not found',
+                ], 400);
+            }
+            $post->update(['liked' => $input['liked']]);
+            return response()->json([
+                'status' => true,
+                'message' => 'Success',
+                'data' => $post
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => $e
+            ], 500);
+        }
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::prefix('post')->group(function () {
+    Route::get('/', [PostController::class, 'index']);
+    Route::post('/create', [PostController::class, 'store']);
+    Route::get('/get-info-client', [PostController::class, 'create']);
+    Route::put('/{post}', [PostController::class, 'update']);
 });
